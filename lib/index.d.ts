5f23a80a76dda22b215ab60010adba0a8b698f13
Sergey Batukov <sashachepuha@gmail.com>
2022-02-24 13:03:29 +0300

    fix

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: {
-    rgba({ r, g, b, a }: {
-        r?: number | undefined;
-        g?: number | undefined;
-        b?: number | undefined;
-        a?: number | undefined;
-    }): string;
-};
-export default _default;
